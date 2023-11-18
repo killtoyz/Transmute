5cd7e97cd4d34acc7a00a109938c733ea2bb94c0
killtoyz <killtoyzmail@gmail.com>
2023-11-18 18:29:08 +0300

    feat: перемещение по каталогам, получение путей файлов, папок

---
+++ Transmute/PathEndingEntity.cs
@@ -0,0 +1,20 @@
+﻿public enum PathEndingEntityEnum
+{
+    File,
+    Directory,
+    NotExist
+}
+
+namespace Transmute
+{
+    public static class PathEndingEntity
+    {
+        public static PathEndingEntityEnum CheckEndingEntity(string path)
+        {
+            if (File.Exists(path)) return PathEndingEntityEnum.File;
+            if (Directory.Exists(path)) return PathEndingEntityEnum.Directory;
+
+            return PathEndingEntityEnum.NotExist;
+        }
+    }
+}
--- Transmute/Program.cs
+++ Transmute/Program.cs
@@ -5,6 +5,76 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+
+            UserInputStringParser userInputParser = new UserInputStringParser();
+
+            var userInputLine = $"{Environment.UserName}:> ";
+            var path = string.Empty;
+
+            var firstTimeEnterCycle = true;
+
+            while (true)
+            {
+                if (firstTimeEnterCycle)
+                {
+                    path = Console.ReadLine() + Path.DirectorySeparatorChar;
+                    userInputParser.Parse(path);
+
+                    path = userInputParser.Path;
+                }
+                else
+                {
+                    var continueInput = Console.ReadLine();
+
+                    if (continueInput.StartsWith("..")) path = DirectoryAndFileInfo.GetParentDirectory(path);
+                    else path = Path.Combine(path, continueInput);
+
+                    userInputParser.Parse(path);
+                    path = userInputParser.Path;
+                }
+
+                var pathEndingEntity = PathEndingEntity.CheckEndingEntity(path);
+
+                if (pathEndingEntity == PathEndingEntityEnum.Directory)
+                {
+                    firstTimeEnterCycle = false;
+
+                    var listOfDirectories = DirectoryAndFileInfo.GetAllDirectories(path);
+                    var listOfJsonFiles = DirectoryAndFileInfo.GetAllFiles(path);
+
+                    try
+                    {
+                        foreach (var dir in listOfDirectories)
+                        {
+                            Console.Write($"{dir.Name} ");
+                            Console.WriteLine();
+                        }
+
+                        foreach (var file in listOfJsonFiles)
+                        {
+                            Console.Write($"{file.Name} ");
+                            Console.WriteLine();
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+
+                    }
+
+                    Console.WriteLine();
+                }
+                else if (pathEndingEntity == PathEndingEntityEnum.File)
+                {
+
+                }
+                else
+                {
+                    userInputParser.Clear();
+                    path = userInputParser.Path;
+                    firstTimeEnterCycle = false;
+                }
+            }
         }
     }
 }

ea9ec029caf0cf56c6f9a4d063b0a2e9a31a4e8c
killtoyz <killtoyzmail@gmail.com>
2023-11-27 17:21:33 +0300

    refactor: инкапсуляция обработки пути в свойстве Path класса UserInputStringParser

--- Transmute/UserInputStringParser.cs
+++ Transmute/UserInputStringParser.cs
@@ -4,15 +4,25 @@
     {
         private static char[] separators = { ' ' };
         public string? UserPreviousCommand { get; private set; }
-        private string? _path;
 
+        private string? _path;
         public string Path
         {
             get
             {
-                if (_path == null) return "";
+                if (_path == null) { return ""; }
                 if (!_path.EndsWith("\\")) return _path + System.IO.Path.DirectorySeparatorChar;
                 return _path;
+            }
+
+            private set
+            {
+                if (value == "..")
+                    _path = DirectoryAndFileManage.GetParentDirectory(_path);
+                else if (_path != null)
+                    _path = System.IO.Path.Combine(_path, value);
+                else
+                    _path = value;
             }
         }
         public string[]? Commands { get; private set; }
@@ -21,7 +31,7 @@
             var parts = input.Split(separators).ToList();
             UserPreviousCommand = string.Join(' ', parts);
 
-            _path = parts[0];
+            Path = parts[0];
             parts.RemoveAt(0);
 
             Commands = parts.ToArray();
